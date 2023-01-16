7e292583c6a1ea1c9f59a3be112d37fb257f7c4c
don336 <smgmartinez92@gmail.com>
2023-01-16 06:12:29 +0300

    initial commit

---
+++ common/header.php
@@ -0,0 +1,7 @@
+
+<ul class="head">
+    <li>
+        <img src="./images/site/logo.png" alt="myImage">
+    </li>
+    <li>My Account</li>
+</ul>
---
+++ template.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" media="screen">
+    <title>PHP Motors</title>
+</head>
+<body>
+    <!-- <img src="./images/site/checkerboard.jpg" alt="mi"> -->
+    <div class="main-container">
+        <div class="container">
+            <header id="main-header">
+                <?php require_once $_SERVER['DOCUMENT_ROOT'].'.\phpmotors\common\header.php'?>
+            <header id="main-header">
+            <nav class="main-nav">
+                <ul class="navbar">
+                    <li>Home</li>
+                    <li><span class="currentpage">Classic</span> </li>
+                    <li>Sports</li>
+                    <li>SUV</li>
+                    <li>Trucks</li>
+                    <li>Used</li>
+                </ul>
+            </nav>
+            <main id="showcase">
+                Content Title Here
+            </main>
+            <footer id="main-footer">
+                <p>@PHP Motors, All rights Reserved.</p>
+                <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed</p>
+            </footer>
+        </div>
+    </div>
+
+</body>
+</html>
